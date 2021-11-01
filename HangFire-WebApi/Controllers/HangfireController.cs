fcdf4fa50bdf07202342272d38940a4856666cd4
TRAVELCTM\avishek.chowdhury <avishek.chowdhury@travelctm.com>
2021-11-01 20:21:29 +1100

    Add project files.

---
+++ HangFire-WebApi/Controllers/HangfireController.cs
@@ -0,0 +1,77 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFire_WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HangfireController : ControllerBase
+    {
+      [HttpGet]
+      public IActionResult Get()
+      {
+         return Ok("Hello from hangfire!!");
+      }
+
+      #region Fire and Forget Job
+      [HttpPost]
+      [Route("[action]") ]
+      public IActionResult Welcome()
+      {
+            var jobId = BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome to our App"));
+            return Ok($"JobId :{jobId}. Welcome email sent to the user");
+      }
+        public void SendWelcomeEmail(string text)//private will throw exception
+        {
+            Console.WriteLine(text);
+        }
+        #endregion
+
+      #region Delay Job
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Discount()
+        {
+            var timeInSeconds = 60;
+            var jobId = BackgroundJob.Schedule(() => SendDiscountEmail("Welcome to our App"), TimeSpan.FromSeconds(timeInSeconds));
+            return Ok($"JobId :{jobId}. Welcome email sent to the user");
+        }
+        public void SendDiscountEmail(string text)//private will throw exception
+        {
+            Console.WriteLine(text);
+        }
+        #endregion
+
+      #region Recurring Jobs
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult DatabaseUpdate()
+        {
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Database updated!"),Cron.Minutely);
+            return Ok("Database check job initiated!");
+        }
+
+        #endregion
+
+      #region Continious Jobs
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Confirm()
+        {
+            var timeInSeconds = 30;
+            var parentJobId = BackgroundJob.Schedule(() => SendUnsubscribeEmail("You asked to be unsubscibed!"), TimeSpan.FromSeconds(timeInSeconds));
+            BackgroundJob.ContinueJobWith(parentJobId, () => Console.WriteLine("You were Unsubscribed!"));
+            return Ok("Confirmation job created!");
+        }
+        public void SendUnsubscribeEmail(string text)//private will throw exception
+        {
+            Console.WriteLine(text);
+        }
+        #endregion
+    }
+}
---
+++ HangFire-WebApi/Startup.cs
@@ -0,0 +1,61 @@
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFire_WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(x => x.UseSqlServerStorage(@"Data Source=B1LN2F3-SYD\SQLEXPRESS;Initial Catalog=hangfire-webapi-db;Integrated Security=True;Pooling=False"));
+            services.AddHangfireServer();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HangFire_WebApi", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HangFire_WebApi v1"));
+            }
+
+            app.UseHttpsRedirection();            
+            app.UseHangfireDashboard();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
